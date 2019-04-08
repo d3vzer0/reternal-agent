783b2ee55a30ccbe7bfbb1419083649b7b3b6901
Joey Dreijer <joey.dreijer@ing.nl>
2019-04-08 03:08:51 +0200

    Changed config with default values when run locally

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ import os
 
 config = {
     "golang": {
-        "src": os.environ["GO_SRC"],
-        "dst": os.environ["GO_DST"],
+        "src": os.getenv("GO_SRC", "./agent/src"),
+        "dst": os.getenv("GO_DST", "./agent/dist"),
     },
     "celery": {
-        "broker": os.environ["CELERY_BROKER"],
-        "results": os.environ["CELERY_BACKEND"],
+        "broker": os.getenv("CELERY_BROKER", "redis://localhost:6379"),
+        "results": os.getenv("CELERY_BACKEND", "redis://localhost:6379"),
         "routes": {
             'agent.*': {
                 'queue': 'agent'
