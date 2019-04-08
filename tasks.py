53dd005c46be4cd3bc911c1175a671ac8ecfd61a
Joey Dreijer <joey.dreijer@ing.nl>
2019-04-08 02:37:30 +0200

    Target hash as output filename

--- tasks.py
+++ tasks.py
@@ -6,6 +6,7 @@ from logger import stdlog
 import os
 import shlex
 import json
+import hashlib
 import subprocess
 import base64
 
@@ -17,26 +18,26 @@ stdlog.info("Started agent building daemon")
 
 @build_app.task(name="agent.build")
 def build_agent(platform, arch, base_url, build_id):
-    stdlog.info("Building: %s-%s %s" %(platform, arch, base_url)) 
-    os.environ["GOOS"] = platform
-    os.environ["GOARCH"] = arch
-    escaped_url = shlex.quote(base_url)
-
-    custom_build_url = '-X main.base_url=%s' %(escaped_url)
-    build_id += ".exe" if platform == "windows" else ""
-    build_path = "%s/%s" %(config["golang"]["dst"], build_id)
-    src_code = "%s/corebeacon.go" %(config["golang"]["src"])
+    stdlog.info('Building: {0}-{1} {2}'.format(platform, arch, base_url)) 
+    os.environ['GOOS'] = shlex.quote(platform)
+    os.environ['GOARCH'] = shlex.quote(arch)
+    custom_build_url = '-X main.base_url={0}'.format(shlex.quote(base_url))
+    build_path = '{0}/{1}'.format(config['golang']['dst'],
+        hashlib.md5('{}-{}'.format(platform, arch).encode()).hexdigest())
+    src_code = '{0}/corebeacon.go'.format(config['golang']['src'])
 
     try:
-        build_output = subprocess.check_output(["go", "build", "-ldflags", custom_build_url, "-o", build_path, src_code])
-        build_data = open(build_path, "rb").read() 
-        build_encoded = base64.b64encode(build_data).decode('utf-8')
-        stdlog.info("Finished building: %s-%s %s" %(platform, arch, base_url)) 
-        result = {"result":"success", "data":"Succesfully built agent", "file":build_encoded}
+        build_output = subprocess.check_output(['go', 'build', '-ldflags',
+            custom_build_url,'-o', build_path, src_code])
+        with open(build_path, 'rb') as build_file:
+            build_data = build_file.read()
+            build_encoded = base64.b64encode(build_data).decode('utf-8')
+            stdlog.info('Finished building: {0}-{1} {2}'.format(platform, arch, base_url)) 
+            result = {"result":"success", "data":"Succesfully built agent", "file":build_encoded}
 
 
     except Exception as err:
-        stdlog.info("Failed to build: %s-%s %s" %(platform, arch, base_url)) 
+        stdlog.info('Failed to build: {0}-{1} {2}'.format(platform, arch, base_url)) 
         result = {"result":"failed", "data":"Unable to run subprocess"}
 
     return result
